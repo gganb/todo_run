632caebd0e0a74ddff26f827936d04cac2574665
gganb <gksql0801@naver.com>
2025-05-08 11:41:02 +0900

    Refactor(exception) : 에러 해결로 인한 로그 제거

--- src/main/java/com/example/todo_run/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo_run/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,11 @@
 package com.example.todo_run.common.exception;
 
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -20,10 +14,8 @@ public class GlobalExceptionHandler {
         return ExceptionResponse.dtoResponseEntity(e.getErrorCode());
     }
 
-    //TODO : valid 예외처리 안됨
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ExceptionResponse> handleValidException(MethodArgumentNotValidException e) {
-        log.info("valid");
         HttpStatus status = HttpStatus.BAD_REQUEST;
         String code = "VALIDATION_FAILED";
         String message = e.getBindingResult().getFieldError().getDefaultMessage();
