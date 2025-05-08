0d9b75c031fe5ad32a9a1b2a0317833d19e13d7d
gganb <gksql0801@naver.com>
2025-05-08 11:42:26 +0900

    feat(schedule) : 에러코드 추가 - SCHEDULE_UPDATE_NO_FIELDS : 입력값이 하나 이상 존재해야함 - TITLE_IS_EMPTY : 제목에 공백 금지

--- src/main/java/com/example/todo_run/domain/schedule/exception/ScheduleError.java
+++ src/main/java/com/example/todo_run/domain/schedule/exception/ScheduleError.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum ScheduleError implements ErrorCode {
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "S01", "일정을 찾을 수 없습니다."),
     SCHEDULE_MODIFY_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "S02", "본인의 일정만 수정할 수 있습니다."),
-    SCHEDULE_DELETE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "S03", "본인의 일정만 삭제할 수 있습니다.");
+    SCHEDULE_DELETE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "S03", "본인의 일정만 삭제할 수 있습니다."),
+    SCHEDULE_UPDATE_NO_FIELDS(HttpStatus.BAD_REQUEST, "S04", "변경할 제목 또는 내용을 하나 이상 입력해주세요."),
+    TITLE_IS_EMPTY(HttpStatus.BAD_REQUEST,"S05","제목은 공백이 될 수 없습니다.");
 
 
     private final HttpStatus status;
