74ec5b1fe78b37d611a7c1dcda89b1b73a0b2c14
gganb <gksql0801@naver.com>
2025-05-08 11:51:50 +0900

    fix(schedule) : 댓글 포함 여부에 따른 반환 DTO 분리 - 댓글 목록이 포함되는 응답에 ScheduleCommentListResponseDto 사용 - 기본 목록 응답은 ScheduleResponseDto 유지

--- src/main/java/com/example/todo_run/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/example/todo_run/domain/schedule/service/ScheduleService.java
@@ -3,9 +3,9 @@ package com.example.todo_run.domain.schedule.service;
 import com.example.todo_run.domain.schedule.dto.request.SaveScheduleRequestDto;
 import com.example.todo_run.domain.schedule.dto.request.UpdateScheduleRequestDto;
 import com.example.todo_run.domain.schedule.dto.response.SaveScheduleResponseDto;
+import com.example.todo_run.domain.schedule.dto.response.ScheduleCommentListResponseDto;
 import com.example.todo_run.domain.schedule.dto.response.ScheduleResponseDto;
 import com.example.todo_run.domain.schedule.dto.response.UpdateScheduleResponseDto;
-import com.example.todo_run.domain.schedule.entity.Schedule;
 
 import java.util.List;
 
@@ -14,11 +14,10 @@ public interface ScheduleService {
 
     List<ScheduleResponseDto> findAllSchedule(Long writerId);
 
-    ScheduleResponseDto findSchedule(Long scheduleId);
+    ScheduleCommentListResponseDto findSchedule(Long scheduleId);
 
     UpdateScheduleResponseDto updateSchedule(Long writerId, Long scheduleId, UpdateScheduleRequestDto requestDto);
 
     void deleteSchedule(Long writerId, Long scheduleId);
 
-    Schedule findByIdOrElseThrow(Long scheduleId);
 }
